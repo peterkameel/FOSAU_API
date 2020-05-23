c6fa56d68401f7b25fa82f8a1582819be1f3d8b4
peterkameel <peterkameel95@gmail.com>
2020-05-23 06:43:06 +0200

    first commit login && signup

--- app.js
+++ app.js
@@ -1,5 +1,23 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+const body = require('body-parser');
+const user = require('./api/user');
+const api = require('./api/api');
+
+mongoose.connect("mongodb+srv://peterkameel:@peter2020@fosau-jyv6o.mongodb.net/fosau?retryWrites=true&w=majority",{
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+});
+
+app.use(body.urlencoded({extended: false}));
+app.use(body.json());
+
+
+//use app api
+app.use('/user', user);
+app.use('/api', api);
+
 
 app.use('/',(req,res)=>{
     res.json({massage: 'Hello peter app is perfict'});
