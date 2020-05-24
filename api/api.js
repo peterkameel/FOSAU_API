6e78b315b2b0deafbf14e2639abfc34c53da0420
peterkameel <peterkameel95@gmail.com>
2020-05-24 04:17:55 +0200

    secound commit restore && sync

--- api/api.js
+++ api/api.js
@@ -1,7 +1,54 @@
 const express = require('express');
-const model   = require('../models/api_model');
+const api   = require('../models/api_model');
 const router  = express.Router();
 
+//fun for restore data
+router.post('/restore', (req, res) => {
+    if (Object.keys(req.body).length !== 0) {
+        api.restoreSemesters(req, res);
+    } else {
+        res.json({
+            error: true,
+            massage: 'Error Try Again'
+        });
+    };
+});
+
+//fun for restore data
+router.post('/syncs', (req, res) => {
+    if (Object.keys(req.body).length !== 0) {
+        for (const item of req.body) {
+            api.syncs(item);
+        };
+        res.json({
+            error: false,
+            massage: 'sync completed'
+        });
+    } else {
+        res.json({
+            error: true,
+            massage: 'Error Try Again'
+        });
+    };
+});
+
+//fun for restore data
+router.post('/syncc', (req, res) => {
+    if (Object.keys(req.body).length !== 0) {
+        for (const item of req.body) {
+            api.syncC(item);
+        };
+        res.json({
+            error: false,
+            massage: 'sync completed'
+        });
+    } else {
+        res.json({
+            error: true,
+            massage: 'Error Try Again'
+        });
+    };
+});
 
 
 module.exports = router;
---
+++ models/api_model.js
@@ -0,0 +1,135 @@
+const mongoose = require('mongoose');
+
+//mongoose Schema
+const SemesterSchema = mongoose.Schema({
+    userid: String,
+    name: String,
+    note: String
+}, {
+        versionKey: false,
+        collection: 'semester'
+    });
+
+const CourseSchema = mongoose.Schema({
+    userid: String,
+    semesterid: String,
+    name: String,
+    deg: String,
+    hour: String
+}, {
+    versionKey: false,
+        collection: 'course'
+    });
+
+//mongoose model
+const Semester = mongoose.model('semester', SemesterSchema);
+const Course = mongoose.model('course', CourseSchema);
+
+//restore function
+exports.restoreSemesters = (req, res) => {
+    var cour;
+    restoreCourses(req, (resultt) => {
+        cour = resultt;
+    });
+    Semester.find({ userid: req.body.userid })
+        .exec()
+        .then(result => {
+            if (result.length > 0) {
+                res.json({
+                    error: false,
+                    massage: 'Backup Restored',
+                    semester: result,
+                    course: cour
+                });
+            }
+            else res.json({
+                error: true,
+                massage: 'Backup Not Found',
+            });
+        });
+};
+const restoreCourses = (req, callback) => {
+    Course.find({ userid: req.body.userid })
+        .exec()
+        .then(result => {
+            if (result.length > 0) return callback(result);
+            else return callback(null);
+        });
+};
+
+
+//sync semesters
+const updateSemester = (item) => {
+    const query = { _id: item._id, userid: item.userid };
+    Semester.findOneAndUpdate(query, {
+        name: item.name,
+        note: item.note
+    }).exec().then(ress => {
+        console.log(ress);
+    }).catch(error => { console.log(error) });
+};
+const interSemester = (item) => {
+    const semes = new Semester({
+        userid: item.userid,
+        name: item.name,
+        note: item.note
+    });
+    semes.save().then(resul => {
+        console.log(resul);
+    }).catch(error => { console.log(error) });
+};
+exports.syncs = (item) => {
+    Semester.find({ _id: item._id, userid: item.userid })
+        .exec()
+        .then(result => {
+            if (result.length > 0) {
+                updateSemester(item);
+                console.log(item._id + '  updated');
+            } else {
+                interSemester(item);
+                console.log(item._id + '  saved');
+            };
+        }).catch(error => { console.log(error) });
+};
+
+//sync courses
+const updateCourse = (item) => {
+    const query = { _id: item._id, semesterid: item.semesterid, userid: item.userid };
+    Course.findOneAndUpdate(query, {
+        userid: item.userid,
+        semesterid: item.semesterid,
+        name: item.name,
+        deg: item.deg,
+        hour: item.hour
+    }).exec().then(ress => {
+        console.log(ress);
+    }).catch(error => { console.log(error) });
+};
+const interCourse = (item) => {
+    const cours = new Course({
+        userid: item.userid,
+        semesterid: item.semesterid,
+        name: item.name,
+        deg: item.deg,
+        hour: item.hour
+    });
+    cours.save().then(resul => {
+        console.log(resul);
+    }).catch(error => { console.log(error) });
+};
+exports.syncC = (item) => {
+    Course.find({ _id: item._id, semesterid: item.semesterid, userid: item.userid })
+        .exec()
+        .then(result => {
+            if (result.length > 0) {
+                updateCourse(item);
+                console.log(item._id + '  updated');
+            } else {
+                interCourse(item);
+                console.log(item._id + '  saved');
+            };
+        }).catch(error => { console.log(error) });
+};
+
+
+
